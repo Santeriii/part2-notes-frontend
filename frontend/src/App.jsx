15198de31a2069bb09c4d782bc9531dc96058669
Santeri Einola <santeri.einola@op.fi>
2024-06-02 15:54:53 +0300

    add link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ const App = ({ notes }) => {
   return (
     <div>
       <h1>${`<virus>steal fatal mainframe data</virus>`}</h1>
+      <p><a href='https://forms.gle/CkokY9Pqi1r5xcsM9'>Ilmoittautumislinkki</a></p>
       <img src={logo} alt="logo"/>
     </div>
   )
